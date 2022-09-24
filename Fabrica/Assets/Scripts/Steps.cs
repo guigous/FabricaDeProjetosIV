73b7ea9d0e887dd4925e9280223e2be994a5557a
firebrunan <89662288+firebrunan@users.noreply.github.com>
2022-09-24 17:44:53 -0300

    Sons de Passo colocados na animação

---
+++ Fabrica/Assets/Scripts/Steps.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steps : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] clips;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Step()
+    {
+        AudioClip clip = GetRandomClip();
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        return clips[UnityEngine.Random.Range(0, clips.Length)];
+    }
+}

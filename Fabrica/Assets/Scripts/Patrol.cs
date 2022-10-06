9c040e422eb9045896f11712359661534025ad31
Guilherme Oliveira <gui_1810@hotmail.com>
2022-10-06 16:29:37 -0300

    Nada demais

--- Fabrica/Assets/Scripts/Patrol.cs
+++ Fabrica/Assets/Scripts/Patrol.cs
@@ -5,8 +5,8 @@ using UnityEngine.AI;
 
 public class Patrol : MonoBehaviour
 {
-    public Transform[] points;
-    private int destPoint = 0;
+    public List<GameObject> pointsLst = new List<GameObject>();
+    private int destPoint;
     private NavMeshAgent agent;
 
     // Start is called before the first frame update
@@ -15,7 +15,7 @@ public class Patrol : MonoBehaviour
         agent=GetComponent<NavMeshAgent>();
         agent.autoBraking = false;
         GotoNextPoint();
-        
+        AddPointstoList();
     }
 
     // Update is called once per frame
@@ -24,21 +24,30 @@ public class Patrol : MonoBehaviour
         // Choose the next destination point when the agent gets
         // close to the current one.
         if (!agent.pathPending && agent.remainingDistance < 0.5f)
-            GotoNextPoint();
+        GotoNextPoint();
+        
 
     }
 
     void GotoNextPoint()
     {
         // Returns if no points have been set up
-        if (points.Length == 0)
+        if (pointsLst.Count == 0)
             return;
 
         // Set the agent to go to the currently selected destination.
-        agent.destination = points[destPoint].position;
+        agent.destination = pointsLst[destPoint].transform.position;
 
         // Choose the next point in the array as the destination,
         // cycling to the start if necessary.
-        destPoint = (destPoint + 1) % points.Length;
+        destPoint = (destPoint + 1) % pointsLst.Count;
+    }
+    void AddPointstoList()
+    {
+        for (int i = 0; i < pointsLst.Count; i++)
+        {
+            pointsLst.Add(GameObject.FindGameObjectWithTag("Point"));
+        }
+    
     }
 }
